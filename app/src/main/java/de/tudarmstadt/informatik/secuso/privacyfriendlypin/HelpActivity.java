da311a5d3d1730cec6ce88255368647e43ee84a6
Karola Marky <karola@pinyto.de>
2015-10-26 22:40:15 +0100

    Menu structure

---
+++ app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/HelpActivity.java
@@ -0,0 +1,9 @@
+package de.tudarmstadt.informatik.secuso.privacyfriendlypin;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by yonjuni on 26.10.15.
+ */
+public class HelpActivity extends ActionBarActivity {
+}
