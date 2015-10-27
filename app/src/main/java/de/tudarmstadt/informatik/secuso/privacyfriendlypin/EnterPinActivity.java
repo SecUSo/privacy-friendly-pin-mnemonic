1fe0b77617d09f8824bd20a09a28461dd0e725a7
Karola Marky <karola@pinyto.de>
2015-10-27 13:43:33 +0100

    Changing between activities works again

--- app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/EnterPinActivity.java
+++ app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/EnterPinActivity.java
@@ -94,11 +94,11 @@ public class EnterPinActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
-        //}
+        }
 
-       // return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
     public void clickDoneButton() {
--- app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/ShowHintActivity.java
+++ app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/ShowHintActivity.java
@@ -5,12 +5,14 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
  * Created by yonjuni on 26.10.15.
  */
-public class ShowHintActivity extends ActionBarActivity{
+public class ShowHintActivity extends ActionBarActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,4 +29,24 @@ public class ShowHintActivity extends ActionBarActivity{
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#024265")));
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
