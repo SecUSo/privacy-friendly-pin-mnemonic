a45711826e163c6e60c7a65e53a7797b6e99b6dd
Karola Marky <karola@pinyto.de>
2015-11-10 16:03:44 +0100

    Styles and structure for tutorials added

---
+++ app/src/main/java/de/tudarmstadt/informatik/secuso/privacyfriendlypin/tutorial/ShowHintTutorialActivity.java
@@ -0,0 +1,9 @@
+package de.tudarmstadt.informatik.secuso.privacyfriendlypin.tutorial;
+
+import android.app.Activity;
+
+/**
+ * Created by yonjuni on 10.11.15.
+ */
+public class ShowHintTutorialActivity extends Activity {
+}
