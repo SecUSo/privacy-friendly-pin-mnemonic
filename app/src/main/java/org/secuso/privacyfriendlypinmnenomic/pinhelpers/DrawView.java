d508132151b42d77384ae85606a46e9b628c9ac0
Karola Marky <karola@pinyto.de>
2015-11-26 15:22:21 +0100

    Apparently, it's easier to convert the zero to an 11 to draw arrow heads, now all heads are drawn correctly

--- app/src/main/java/org/secuso/privacyfriendlypinmnenomic/pinhelpers/DrawView.java
+++ app/src/main/java/org/secuso/privacyfriendlypinmnenomic/pinhelpers/DrawView.java
@@ -63,12 +63,19 @@ public class DrawView extends View {
     public int whichArrow(View view1, View view2, int first, int second) {
 
 
+        if (first == 0) {
+            first = 11;
+        }
+        if (second == 0) {
+            second = 11;
+        }
+
         //Arrow should point from right to left
         if ((((first > second) || (second == 1)) && (view1.getY() == view2.getY())) || (first == 2 || first == 3) && second == 1) {
             return 180;
         }
         // Arrow should point downwards
-        if ((((first < second) && first != 0) || (second == 0)) && (view1.getX() == view2.getX())) {
+        if (((first < second)) && (view1.getX() == view2.getX())) {
             return 90;
         }
         // Arrow should point upwards
@@ -76,39 +83,37 @@ public class DrawView extends View {
             return 270;
         }
         //Arrow should point down from left to right
-        if (((first < second) || second == 0) && (second - first == 4 || second - first == 8) || first - second == 7) {
+        if (((first < second)) && (((second - first == 4) || second - first == 8) || first - second == 7)) {
             return 45;
         }
         //Arrow should point down from right to left
-        if (((first < second) || second == 0) && (second - first == 4 || second - first == 2) || first - second == 9) {
+        if (((first < second)) && (second - first == 4 || second - first == 2) || first - second == 9) {
             return 135;
         }
         //Arrow should point up from left to right
-        if (((first > second) || first == 0) && ((first - second == 4 && first ==7) || first - second == 2) || second - first == 9) {
+        if (((first > second)) && ((first - second == 4 && first == 7) || first - second == 2) || second - first == 9) {
             return -45;
         }
         //Arrow should point up from right to left
-        if ((((first > second) || first == 0)) && ((first - second == 4 || first - second == 8) || second - first == 7)) {
+        if ((((first > second)) && ((first - second == 4 || first - second == 8) || second - first == 7))) {
             return -135;
         }
-        //Arrow should point down angularly from left to right TODO Arrow from 4 to 0
-        if (((first < second) && (second - first == 7)) || (first == 4 && second == 0)) {
+        //Arrow should point down angularly from left to right
+        if (((first < second) && (second - first == 7)) || (first == 4 )) {
             return 67;
         }
         //Arrow should point down angularly from right to left
-        if (((first < second) && (second - first == 5)) || (first == 6 && second == 0)) {
+        if (((first < second) && (second - first == 5)) || (first == 6 )) {
             return 112;
         }
         //Arrow should point up angularly from left to right
-        if (((first > second) && (first - second == 7)) || (second == 4 && first == 0)) {
+        if (((first > second) && (first - second == 7)) || (second == 4 )) {
             return -112;
         }
-        //Arrow should point up angularly from right to left TODO Arrow from 4 to 0
-        if (((first > second) && (first - second == 5)) || (second == 6 && first == 0)) {
+        //Arrow should point up angularly from right to left
+        if (((first > second) && (first - second == 5)) || (second == 6)) {
             return -67;
         }
-
-
         return 0;
     }
 
