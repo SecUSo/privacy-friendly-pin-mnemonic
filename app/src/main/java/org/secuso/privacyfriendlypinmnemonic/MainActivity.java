22eb29c56461ab99db2226f116b5b79ed1caf55d
Karola Marky <yonjuni@pinyto.de>
2016-11-04 00:36:01 +0200

    Structure for all dialogs added

--- app/src/main/java/org/secuso/privacyfriendlypinmnemonic/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlypinmnemonic/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends BaseActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setView(i.inflate(R.layout.dialog_mnemonics, null));
             builder.setIcon(R.mipmap.icon);
-            builder.setTitle(getActivity().getString(R.string.welcome));
+            builder.setTitle(getActivity().getString(R.string.tutorial_mnemonics_title));
             builder.setPositiveButton(getActivity().getString(R.string.okay), null);
             builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
                 @Override
@@ -100,7 +100,7 @@ public class MainActivity extends BaseActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setView(i.inflate(R.layout.dialog_reset, null));
             builder.setIcon(R.mipmap.icon);
-            builder.setTitle(getActivity().getString(R.string.welcome));
+            builder.setTitle(getActivity().getString(R.string.security_reset_title));
             builder.setPositiveButton(getActivity().getString(R.string.okay), null);
             builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
                 @Override
@@ -112,6 +112,34 @@ public class MainActivity extends BaseActivity {
             return builder.create();
         }
     }
+
+    public static class PraticeDialog extends DialogFragment {
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            LayoutInflater i = getActivity().getLayoutInflater();
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setView(i.inflate(R.layout.dialog_reset, null));
+            builder.setIcon(R.mipmap.icon);
+            builder.setTitle(getActivity().getString(R.string.tutorial_practice_title));
+            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
+            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ((MainActivity)getActivity()).goToNavigationItem(R.id.nav_help);
+                }
+            });
+
+            return builder.create();
+        }
+    }
+
 
 
 
