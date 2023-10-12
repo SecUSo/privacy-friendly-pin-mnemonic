eea99aa6b98631d5279952e7c1912ef94b19eb2c
Patrick Schneider <urulu@student.kit.edu>
2023-10-12 09:56:26 +0200

    Migrates to AndroidX.

--- app/src/main/java/org/secuso/privacyfriendlypinmnemonic/SplashActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlypinmnemonic/SplashActivity.java
@@ -2,7 +2,7 @@ package org.secuso.privacyfriendlypinmnemonic;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Created by karo on 03.11.16.
